b39be94ef45d3549007f627d0ee00da2f190955f
Mariano Simone <marianosimone@gmail.com>
2022-04-04 22:17:12 -0600

    Detect audiobooks as books
    Closed #5

--- process.py
+++ process.py
@@ -95,7 +95,7 @@ def extract_subproduct(subproduct: dict[str, Any]) -> Optional[Item]:
         return None
     if type_of_item == ["other"]:
         return PrintableModel(name, icon, url, recommended)
-    if type_of_item == ["ebook"]:
+    if type_of_item == ["ebook"] or type_of_item == ["audiobook"]:
         return Book(name, icon, url, subproduct["payee"]["human_name"], recommended)
     if type_of_item == ["audio"]:
         return Soundtrack(name, icon, url, recommended)
@@ -111,8 +111,20 @@ def extract_subproduct(subproduct: dict[str, Any]) -> Optional[Item]:
     return None
 
 
-def detect_type(downloads: list[dict[str, str]]) -> list[str]:
-    return [d["platform"] for d in downloads]
+def detect_type(downloads: list[dict[str, Any]]) -> list[str]:
+    platforms = [d["platform"] for d in downloads]
+    if platforms == ["audio"]:
+        all_links = chain.from_iterable(
+            map(
+                lambda d: chain.from_iterable(
+                    [l["url"].values() for l in d["download_struct"]]
+                ),
+                downloads,
+            )
+        )
+        if any(map(lambda url: "audiobook" in url, all_links)):
+            return ["audiobook"]
+    return platforms
 
 
 def extract_steam_game(tpkd: dict[str, str]) -> Optional[str]:
